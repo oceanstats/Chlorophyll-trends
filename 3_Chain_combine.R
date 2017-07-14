c3d400c4960bf6407fe8f9fea7f208f2fcd6e561
oceanstats <thistrove@yahoo.co.uk>
2017-07-15 00:32:10 +0100

    Update 3_Chain_combine.R

--- 3_Chain_combine.R
+++ 3_Chain_combine.R
@@ -51,8 +51,28 @@ if(j %in% c(1,2,4,6,7,8,10,11,12,13,17,18,19,22,25,26,27,29,30,31,34,35,36,40,43
   fitted[,1] <- temp2
   
 
-  #converting 1d fitted chl to 3d (lat, lon, mon)
-fittedChl <- aperm(array(exp(fitted),dim=c(196,360,180)),c(2,3,1))
+ #converting 1d fitted chl to 3d (lat, lon, mon)
+  time <- 1:196
+
+lonind <- which(model_input$Longitude>179.5)
+model_input$Longitude[lonind] <- model_input$Longitude[lonind]-360# removing original correction for having longitudes increasing continuously
+
+  #locations same for all time steps--> work out locations for plugging into fittedChl
+  loc <- model_input[seq(1,length(model_input$Longitude),196),2:3] 
+  locind <- array(NA,dim=c(nrow(loc),2))
+  for(m in 1:nrow(loc))
+{
+  locind[m,1] <- which(lons==loc[m,1])
+  locind[m,2] <- which(lats==loc[m,2])
+}
+  for(k in 1:196)
+{
+fittemp <- exp(fitted[seq(1+(k-1),length(model_input$Longitude),196),1])
+for(m in 1:length(fittemp))
+{
+fittedChl[locind[m,1],locind[m,2],k] <- fittemp[m]
+}
+  }
 
 }
 }
