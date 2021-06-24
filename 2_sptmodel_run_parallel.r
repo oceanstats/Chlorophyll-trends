e39061b33366300ebba801ddb4f51fce6838d875
oceanstats <thistrove@yahoo.co.uk>
2021-06-25 11:02:24 +1200

    Update 2_sptmodel_run_parallel.r

--- 2_sptmodel_run_parallel.r
+++ 2_sptmodel_run_parallel.r
@@ -21,7 +21,7 @@ area <- area[-c(1,2,4,6,7,8,10,11,12,13,17,18,19,22,25,26,27,29,30,31,34,35,36,4
 
 part <- seq(1,41,1)
 ######create function for running burn in
-dothisburn <- function(j){
+run_burn <- function(j){
 savename <- paste0(j,"_BGC_input.Rdata")
 load(savename)
 
@@ -33,13 +33,13 @@ Model<- spT.Gibbs(
   data=spTmodel, model="GPP",
   coords=~Longitude+Latitude,knots.coords=knotgrid,
   distance.method="geodetic:km", time.data=time.data, report=1,
-  nItr =1000, nBurn=0,  scale.transform="LOG",
+  nItr =8000, nBurn=0,  scale.transform="LOG",
   priors=spT.priors(model="GPP", inv.var.prior=Gamm(a=2,b=1),
   beta.prior=Norm(0,10^2), rho.prior=Norm(0,10^2)),
   spatial.decay=spT.decay(distribution="FIXED", value=(3/1500)))
 
 
-  savename <- paste0(j,"_BGC_burn.Rdata")
+  savename <- paste0(j,"_BGC_model_pt0.Rdata")
   
 
   #save used components of model (including what was previously saved in sptmodel input)
@@ -61,9 +61,8 @@ Model<- spT.Gibbs(
 run_parallel <- function(i,j){
 savename <- paste0(j,"_BGC_input.Rdata")
 load(savename)
-if(i>1)
-{
-  savename <- paste0(j,"_BGC_model_pt",i-1,".Rdata")#load last iteration
+
+savename <- paste0(j,"_BGC_model_pt",i-1,".Rdata")#load last iteration
 load(savename)
 
 ####Subsequent sets of model iterations
@@ -74,7 +73,7 @@ Model<- spT.Gibbs(
   data=spTmodel, model="GPP",
   coords=~Longitude+Latitude,knots.coords=knotgrid,
   distance.method="geodetic:km", time.data=time.data, report=1,
-  nItr =1000, nBurn=0,  scale.transform="LOG", initials=init,
+  nItr =8000, nBurn=0,  scale.transform="LOG", initials=init,
   priors=spT.priors(model="GPP", inv.var.prior=Gamm(a=2,b=1),
   beta.prior=Norm(0,10^2), rho.prior=Norm(0,10^2)),
   spatial.decay=spT.decay(distribution="FIXED", value=(3/1500)))
@@ -97,48 +96,32 @@ Model<- spT.Gibbs(
   save(betap,fitted,model_input,PMCC,rhop,sig2eps,sig2etap,comp,file=savename)
 
    rm(list=c("Model","betap","fitted","model_input","PMCC","rhop","sig2eps","sig2etap"))
- } else{
- ####First 1000 model iterations
-time.data <- spT.time(t.series=196,segments=1) 
-
-Model<- spT.Gibbs(
-  formula = chl ~ TT+SST+M,
-  data=spTmodel, model="GPP",
-  coords=~Longitude+Latitude,knots.coords=knotgrid,
-  distance.method="geodetic:km", time.data=time.data, report=1,
-  nItr =1000, nBurn=0,  scale.transform="LOG", 
-  priors=spT.priors(model="GPP", inv.var.prior=Gamm(a=2,b=1),
-  beta.prior=Norm(0,10^2), rho.prior=Norm(0,10^2)),
-  spatial.decay=spT.decay(distribution="FIXED", value=(3/1500)))
-
-
-  savename <- paste0(j,"_BGC_model_pt",(i-1),".Rdata")
-  
-
-  #save used components of model (including what was previously saved in sptmodel input)
-  betap <- Model$betap
-  fitted <- Model$fitted
-  model_input <- Model$data
-   PMCC <- Model$PMCC
-    rhop <- Model$rhop
-  sig2eps <- Model$sig2eps
-  sig2etap <- Model$sig2etap
-  comp <- Model$computation.time
-      leg <- length(rhop) 
-  init=spT.initials(model="GPP",sig2eps=sig2eps[leg],sig2eta=sig2etap[leg],rho=rhop[leg],beta=betap[,leg])#initials from starting off next leg
-  save(betap,fitted,model_input,PMCC,rhop,sig2eps,sig2etap,comp,file=savename)
-
-   rm(list=c("Model","betap","fitted","model_input","PMCC","rhop","sig2eps","sig2etap"))
- }
 }
  #multi core setup
  
  registerDoParallel(cores=8)
 
- #run subsequent iterations
- foreach(i=part,.packages="spTimer")  %:% 
-foreach(j=area,.packages="spTimer")  %dopar% 
-
+#run burn-in
+foreach(j=area,.packages="spTimer")  %dopar% #running burn in 
 {
-  run_parallel(i,j)
+  run_burn(j)
+}
+
+ #run subsequent iterations
+foreach(j=area,.packages="spTimer")  %dopar% 
+{
+  run_parallel(j,1)
+}
+foreach(j=area,.packages="spTimer")  %dopar% 
+{
+  run_parallel(j,2)
+}foreach(j=area,.packages="spTimer")  %dopar% 
+{
+  run_parallel(j,3)
+}foreach(j=area,.packages="spTimer")  %dopar% 
+{
+  run_parallel(j,4)
+}foreach(j=area,.packages="spTimer")  %dopar% 
+{
+  run_parallel(j,5)
 }
